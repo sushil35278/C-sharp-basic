73ba9c3361fe50bb28bd329c04efbc6645a07047
sigsushil98@gmail.com <sigsushil98@gmail.com>
2022-11-18 13:41:21 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string Name = "Sushil";
+            string LastName = "Sigdel";
+            Console.WriteLine("Hello "+ Name + " "+ LastName);
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x + y);
+            Console.WriteLine("Please enter your name: ");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Welcome " + myName);
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age);
+            */
+        }
+    }
+}
