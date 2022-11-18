a4f83c575c6e1c0109d5b79207fa9034247098c9
sigsushil98@gmail.com <sigsushil98@gmail.com>
2022-11-18 22:12:15 +0545

    some operation are done including type casting, coaleseing and interpolation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Helloworld
 {
@@ -14,15 +10,144 @@ namespace Helloworld
             string Name = "Sushil";
             string LastName = "Sigdel";
             Console.WriteLine("Hello "+ Name + " "+ LastName);
+
+
             int x = 5;
             int y = 6;
             Console.WriteLine(x + y);
+
+
             Console.WriteLine("Please enter your name: ");
             string myName = Console.ReadLine();
             Console.WriteLine("Welcome " + myName);
+
+
             Console.WriteLine("Please enter your age: ");
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(age);
+ 
+
+            int x = 2;
+            int y = 3;
+            Console.WriteLine(x + y);
+
+
+            int x = 1;
+            x++;
+            Console.WriteLine(x);
+
+
+            int x = 3;
+            x += 5;
+            Console.WriteLine(x); 
+
+            int x = 5;
+            x ^= 3;
+            Console.WriteLine(x);
+
+
+            int x = 5;
+            int y = 5;
+            Console.WriteLine(x == y);
+
+
+            int x = 5;
+            int y = 3;
+            Console.WriteLine(x >= y);
+
+            int x = 5;
+            Console.WriteLine(x > 3 && x < 10);
+
+            int x = 5;
+            Console.WriteLine(!(x > 3 && x < 10));
+            
+
+            int x = 5;
+            int y = -3;
+            Math.Min(x, y);
+            Math.Min(x, y);
+            Math.Sqrt(x * x);
+            Math.Abs(y);
+            Math.Round(2.22);
+            
+
+            string txt = "ABCDEFGHIJklmnopqrstuvwxyz";
+            Console.WriteLine("The length of the string is: " + txt.Length);
+            Console.WriteLine(txt.ToLower());
+            Console.WriteLine(txt.ToUpper());
+            
+
+            
+            string firstName = "Sushil";
+            string lastName = "Sigdel";
+            string name = string.Concat(firstName, lastName);
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+            
+            string firstName = "Sushil";
+            string lastName = "Sigdel";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
+            
+
+            int Number = 10;
+            bool isNumber = Number == 10 ? true : false;
+            Console.WriteLine("The condition is {0}", isNumber);
+            
+
+
+            string Name = null;
+            int? i = null;
+            bool? IsYourMajor = null;
+
+            
+            bool? AreYouMajor  = null;
+            if (AreYouMajor == true)
+            {
+                Console.WriteLine("It is major");
+            }
+            else if (AreYouMajor == false)
+            {
+                Console.WriteLine("It is not major");
+            }
+            else
+            {
+                Console.WriteLine("It is not given");
+            }
+
+           
+
+            Console.WriteLine("Enter the number of Tickets on sales");
+            int? TicketsOnSales = Convert.ToInt32(Console.ReadLine());
+            int AvailableTickets;
+            if(TicketsOnSales == null)
+            {
+                AvailableTickets = 0;
+                Console.WriteLine("No tickets are available");
+            }
+            else
+            {
+                AvailableTickets = TicketsOnSales.Value;
+            }
+            Console.WriteLine("Available Tickets are: {0}", AvailableTickets);
+            Console.ReadLine();
+             
+
+
+            Console.WriteLine("Enter the number of Tickets on sales");
+            int? TicketsOnSales = Convert.ToInt32(Console.ReadLine());
+            int AvailableTickets = TicketsOnSales ?? 0;
+            Console.WriteLine("Available Tickets are: {0}", AvailableTickets);
+
+            
+            float i = 1.00F;
+            int j = (int) i;
+            Console.WriteLine(j);
+
+            int i = 10;
+            string Name = Convert.ToString(i);
+            Console.WriteLine(Name);
             */
         }
     }
